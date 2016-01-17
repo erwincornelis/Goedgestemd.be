5a1e9777add48a3139139efbf8662004d85c7cb1
erwincornelis <erwin.cornelis@gmail.com>
2016-01-17 12:44:35 +0100

    Initial import

---
+++ v1.0/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
+    <head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> 
+		<meta name="keywords" content="Coach, Stemcoaching, Training, Stemtraining, Stem, Therapeut, Praktijk, Logopedist, Logopediste, Rekenen, 
+Lezen, Schrijven, Dyslexie, Dysorthografie, Dyscalculie, Presentatie, Microfoon, Mooie stem, Keelpijn, Stemmoe, Rekenproblemen, Leesproblemen, ADHD, CVS, Fibromylagie, Uitspraak, Articulatie, Radio, TV, 
+Televisie, Stemtest, Cameratraining, Leerkracht, Lesgeven, Workshop, Goed Gestemd, Kinderen, Volwassenen, Nasaliteit, Afwijkend mondgedrag, Stem verzorgen, Dialect, Regionale klankkleur, Stembeheersing, Intonatie, 
+Pauze, Klemtonen, Stemdynamiek, Verzorgde uitspraak, Mediatraining, Ademhaling, Heesheid, Stemadvies, Stemhygiëne, Mompelen, Roepen, VRT, VTM, Woestijnvis, Taalachterstand, Slissen, R probleem" />
+	<meta name="description" content="Een Dynamisch Stemgebruik met een Goede Ademhaling en een Mooie Verzorgde Uitspraak geschikt voor Presentaties , Camera en Lesgeven zodat je Goed Gestemd door het Spreken gaat op Radio en Televisie" >
+	<meta name="revisit-after" content="1 month" />
+	<meta name="web_author" content="Erwin Cornelis" />
+	
+	<meta http-equiv="language" content="NL" />
+		
+		
+		<title>Goed gestemd</title> 
+		
+		<!-- Add jQuery library -->
+		<script type="text/javascript" src="jquery/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
+		
+		<!-- email spam protection -->
+		<script type="text/javascript" src="jquery/jquery.emailSpamProtection.1.0.js"></script>
+			
+		<link rel="stylesheet" type="text/css" href="style.css" />
+
+		<script type="text/javascript">
+			jQuery.fn.center = function () {
+				this.css("position","absolute");
+				this.css("left", Math.max(0, (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+				return this;
+			}
+			
+			$(document).ready(function() {
+				$('#container').center();
+				
+				$(window).resize(function() {
+					$('#container').center();
+				});
+				
+				$('body').emailSpamProtection('email');
+			});	   
+
+			$('#container #content-left #subNavigatie ul li a:not(.subNavigatie-active)')
+			.live('mouseover', function() {
+				$(this).stop().animate({paddingLeft: '25px'}, {duration: 400}); 
+			})
+			.live('mouseout', function() {
+				$(this).stop().animate({paddingLeft: '5px'}, {duration: 400}); 
+			});		
+
+		</script>
+		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-35386172-2']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+
+    </head>
+    <body>
+		<div id="container">
+			<div class="tabs">
+				<div id="subNavigatie">	
+					<ul style="list-style-type: none;">
+						<li><a href="#waarom"><img src="images/nav-waarom.png" style="position: absolute; top: 27px; right: 0px; border-width: 0px;" /></a></li>
+						<li><a href="#praktisch"><img src="images/nav-praktisch.png" style="position: absolute; top: 131px; right: 0px; border-width: 0px;" /></a></li>
+						<li><a href="#stem"><img src="images/nav-stem.png" style="position: absolute; top: 248px; right: 0px; border-width: 0px;" /></a></li>
+						<li><a href="#uitspraak"><img src="images/nav-uitspraak.png" style="position: absolute; top: 317px; right: 0px; border-width: 0px;" /></a></li>
+						<li><a href="#presenteren"><img src="images/nav-presenteren.png" style="position: absolute; top: 433px; right: 0px; border-width: 0px;" /></a></li>
+						<li><a href="#boek"><img src="images/nav-boek.png" style="position: absolute; top: 579px; right: 0px; border-width: 0px;" /></a></li>
+					</ul>
+				</div>
+				
+			
+				<div id="content-left" style="float: left; width: 360px; position: absolute; left: 60px; top: 30px; padding: 20px 10px 10px 30px;">
+					<img src="images/GoedGestemd_cover.jpg" style="width: 350px; margin-top: 15px;" />					
+				</div>
+				<div id="content-right" style="float: right; width: 360px; height: 580px; position: absolute; right: 60px; top: 30px; padding: 20px 20px 10px 20px;">			
+					<div id="subContent">
+						<div id="waarom">
+							<h1>Waarom</h1>
+							<p>
+								Waarom moet jij dit boek lezen en vooral toepassen ?<br />
+								Omdat je stem een essentieel onderdeel is van je beroep !<br />
+								Omdat je graag spreekt !<br />
+								Omdat je sociaal bent !<br />
+								Omdat geen stem hebben een heel grote rem is op veel dingen die je doet !<br />
+								Je eigen karakter , je warmte , je eigenheid behouden maar wel met de juiste stem - en klankkleur. Daar ga ik voor ! Een natuurlijke vlotte stem zonder pijn of vermoeidheid maar wel met het juiste gebruik van alle spiertjes zodanig dat je stem nog jaren mee kan gaan. Stemmen die stevig klinken en degelijkheid uitstralen maar toch vriendelijk en warm zijn . Dat is wat ik je wil leren.<br />
+								Presenteren , met mensen communiceren , je boodschap overbrengen.<br />
+								Elke dag ben ik bezig met intonatie , de wijze waarop je de dingen zegt.br />
+								Wat is de strategie van een manager waard als je ze niet weet over te brengen ?<br />
+								Wat kun je in een Call - centrum doen als je niet de juiste toon kunt aanslaan ?<br />
+								Wat doe je anders op radio of televisie ?<br />
+								Hoe doe je dat ?<br />
+								Door de juiste klemtonen te leggen. Door boeiend te spreken. Door variatie in je stem te leggen.<br />
+							</p>
+						</div>
+						<div id="praktisch">
+							<h1>Praktisch</h1>
+							<p>
+								Ik heb een boek gemaakt zoals ik les geef.<br />
+								Gewoon , vertellen wat ik weet , eenvoudig en toegankelijk.<br />
+								Geen 6 jaren plan maar op korte termijn heel veel resultaat boeken.<br />
+								Zelf heb ik een hekel aan theorie . Ik ben een doe mens.<br />
+								Begin eraan , train , leer , probeer , en zie resultaat.<br />
+								Theorie is wel belangrijk , en dat meen ik.<br />
+								Daarom geef ik je overal op een toegankelijke manier (hoop ik) de basiskennis mee.<br />
+								Eerst even een idee krijgen van wat je gaat doen en waarom en dan heel veel oefeningen. Het is maar door het te doen dat je iets zult bereiken.<br />
+								De CD , ingelezen door heel veel verschillende VRT - stemmen , zal je daarbij helpen.<br />
+								Ook daar staan niets anders dan oefeningen op.<br />
+							</p>
+						</div>
+						<div id="stem">
+							<h1>Stem</h1>
+							<p>
+								Jij spreekt veel , je spreekt graag , je houdt ervan voor te lezen of je beroep vraagt dat je je stem veel gebruikt.<br />
+								Wel , dan is dit een goed boek voor jou !<br />
+								Je hoeft niet te wachten tot je een stemprobleem hebt of hees klinkt , je kunt ook preventief aan je stem werken.<br />
+								Ik zie vaak toekomstige journalisten , leerkrachten in spe . Zij gaan binnenkort hun stem heel intensief gebruiken.<br />
+								Bij een professionele sporter leer je toch ook eerst de technieken en daarna ga je pas hoogspringen.<br />
+								Bij stem is dat net zo .<br />
+								In het hoofdstuk stem vind je heel veel tips om je stem te sparen.<br />
+								Is er toch al een miniprobleemje zoals heesheid , dan zijn er veel oefeningen om je snel weer op het goede pad te brengen.<br />
+							</p>
+						</div>
+						<div id="uitspraak">
+							<h1>Uitspraak</h1>
+							<p>
+								Je wilt verzorgd spreken , je regionale klankkleur stoort je soms , je bent leerkracht. Voor jouw is een verzorgde uitspraak heel belangrijk maar nog belangrijker is dat je natuurlijk en gewoon klinkt.<br />
+								Je mag de beste uitspraak van de wereld hebben maar als je hautain klinkt zal niemand naar je luisteren.<br />
+								En daar ga ik voor ! Verzorgd , mooi en natuurlijk vlot praten.<br />
+								Tezamen met de CD doorlopen we verschillende oefeningen die je snel naar een vlotte correct uitspraak brengen.<br />
+							</p>
+						</div>
+						<div id="presenteren">
+							<h1>Presenteren</h1>
+							<p>
+								Vlot en dynamisch je boodschap overbrengen. Daar gaat het over.<br />
+								Of je nu CEO bent , journalist of je moet een voordracht geven , dat maakt niet uit.<br />
+								Iedereen kent wel de saaie Nieuwjaarsspeech waar niemand naar luistert. Maar dat geldt niet voor jou ! Als je de bladzijden en oefeningen over presentatie doorneemt zal je merken dat de juiste klemtonen en dynamiek ook in jou bereik liggen.<br />
+								Het is echt niet zo moeilijk om een tekst zeggend te lezen. Alleen moet je de spelregels even kennen. Eenmaal je die beheerst zal je publiek graag naar je luisteren.<br />
+							</p>
+						</div>
+						<div id="boek">
+							<h1>Boek</h1>
+							<p>
+								In dit boek helpt Christel Lacroix je op weg naar een gezonde en aangenaam klinkende stem. Er is ook een bijhorende cd vol praktische oefeningen , ingelezen door vertrouwde radio - en televisie-stemmen van VRT , zoals Wim De Vilder , Kobe Ilsen , Indra Dewitte , Christophe Lambrecht , Cath Luyten en vele andere. Een toegankelijk boek met leuke oefeningen voor een mooie uitspraak , een gezonde stem en een aangename intonatie en presentatie.
+							</p>
+							<p class="citaat">
+								'Dat ik een fijn televisieprogramma mag presenteren , heb ik te danken aan Christel Lacroix. In pure My Fair lady - stijl veranderde ze mijn plat Antwerps Eliza Doolittle-gemompel in iets verstaanbaars. Net als professor Higgins deden we honderd keer "het Spaanse graan" en gingen we op zoek naar de juiste ij. In een mum van tijd leerde ze me praten &#233;n presenteren.'<br />
+								Erik Van Looy
+							</p>
+							<p class="citaat">
+								'Ik dacht dat ik kon lezen. Tot ik tijdens het journalistenexamen voor een micro een nieuwsbericht moest voorlezen. "Voor verbetering vatbaar" , was het oordeel van de terecht strenge jury. En zo geschiedde , met de hulp en het geduld van Christel Lacroix. Geen mens gelooft me nog als ik dat verhaal van de jury vertel.'<br />
+								Phara de Aguirre
+							</p>
+						</div>
+					</div>
+				</div>
+				
+				<script type="text/javascript">
+					$(function () {
+						var tabContainers = $('div.tabs div#subContent > div');
+								
+						$('div.tabs div#subNavigatie ul a').click(function () {
+							tabContainers.hide();
+							tabContainers.filter(this.hash).show();
+													
+							$('.subNavigatie-active').stop().animate({paddingLeft: '5px'}, {duration: 400}); 
+							$('div.tabs div#subNavigatie ul a').removeClass('subNavigatie-active'); //make other tabs inactive
+							$(this).addClass('subNavigatie-active'); //make this tab active
+							$('.subNavigatie-active').stop().animate({paddingLeft: '25px'}, {duration: 400});
+
+							return false;
+						}).filter('[href$="' + document.location.hash + '"]').click();
+					});
+				</script>
+			</div>
+			<div class="clear"></div>
+			
+			<div id="contact-webdesigner">Website and design by Erwin Cornelis (<span class="email">erwin.cornelis [at] gmail.com</span>)</div>
+		</div>
+		
+		
+    </body>
+</html>
